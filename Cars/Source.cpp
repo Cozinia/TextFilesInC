ef72eec0c9259f8159bc79ab914eb85c7c011eeb
Cozinia <hdwallpaper04@gmail.com>
2023-03-24 15:47:47 +0200

    Searching car functionality added

--- Cars/Source.cpp
+++ Cars/Source.cpp
@@ -53,7 +53,7 @@ Car addCar(Car newCar) {
 	return newCar;
 }
 
-void printCar(Car car) 
+void printAllCars(Car car) 
 {
 	FILE* file = fopen("cars.txt", "r");
 	if (file != NULL)
@@ -72,7 +72,7 @@ void printCar(Car car)
 			fscanf(file, "Was in accident? %s\n", car.wasInAccident);
 
 			
-			printf("Car ID: %d\n", car.carID);
+			printf("\nCar ID: %d\n", car.carID);
 			printf("Make: %s\n", car.make);
 			printf("Model: %s\n", car.model);
 			printf("Year: %d\n", car.year);
@@ -87,6 +87,22 @@ void printCar(Car car)
 	else {
 		printf("Couldn't open cars.txt\n");
 	}
+
+	return;
+}
+
+void printCar(Car car) 
+{
+
+	printf("\nCar ID: %d\n", car.carID);
+	printf("Make: %s\n", car.make);
+	printf("Model: %s\n", car.model);
+	printf("Year: %d\n", car.year);
+	printf("Horsepower: %d\n", car.horsepower);
+	printf("Kilometers: %d\n", car.mileage);
+	printf("Fuel type: %s\n", car.fuelType);
+	printf("Transmission: %s\n", car.gearbox);
+	printf("Was in accident? %s\n", car.wasInAccident);
 
 	return;
 }
@@ -115,42 +131,113 @@ void saveCar(Car car) {
 	return;
 }
 
-void deleteCar(int carID, Car car) {
-	FILE* file = fopen("cars.txt", "w+");
-	if (file != NULL)
+Car readCarFromFile(Car car, FILE* readFile) 
+{
+	fscanf(readFile, "\nCar ID: %d\n", &car.carID);
+	fscanf(readFile, "Make: %s\n", car.make);
+	fscanf(readFile, "Model: %s\n", car.model);
+	fscanf(readFile, "Year: %d\n", &car.year);
+	fscanf(readFile, "Horsepower: %d\n", &car.horsepower);
+	fscanf(readFile, "Kilometers: %d\n", &car.mileage);
+	fscanf(readFile, "Fuel: %s\n", car.fuelType);
+	fscanf(readFile, "Trasmission: %s\n", car.gearbox);
+	fscanf(readFile, "Was in accident? %s\n", car.wasInAccident);
+
+	return car;
+}
+
+void writeInFile(Car car, FILE* writeFile) 
+{
+
+	fprintf(writeFile, "\nCar ID: %d\n", car.carID);
+	fprintf(writeFile, "Make: %s\n", car.make);
+	fprintf(writeFile, "Model: %s\n", car.model);
+	fprintf(writeFile, "Year: %d\n", car.year);
+	fprintf(writeFile, "Horsepower: %d\n", car.horsepower);
+	fprintf(writeFile, "Kilometers: %d\n", car.mileage);
+	fprintf(writeFile, "Fuel: %s\n", car.fuelType);
+	fprintf(writeFile, "Trasmission: %s\n", car.gearbox);
+	fprintf(writeFile, "Was in accident? %s\n", car.wasInAccident);
+
+	return;
+}
+
+//void deleteCar(int carID, Car writeCar) {
+//	FILE* readFile = fopen("cars.txt", "r");
+//	FILE* writeFile = fopen("carsTemp.txt", "w");
+//	FILE* readFromTempFile = fopen("carsTemp.txt", "r");
+//	FILE* writeInOriginalFile = fopen("cars.txt", "w");
+//	if (readFile != NULL)
+//	{
+//		while (!feof(readFile))
+//		{
+//			writeCar = readCarFromFile(writeCar, readFile);
+//			if (writeCar.carID != carID)
+//			{
+//				writeInFile(writeCar, writeFile);
+//
+//			}
+//		}
+//		fclose(readFile);
+//		fclose(writeFile);
+//		printf("\nCar was deleted!");
+//	}
+//	else {
+//		printf("Couldn't open cars.txt\n");
+//	}
+//
+//	if (readFromTempFile != NULL)
+//	{
+//		while (!feof(readFromTempFile))
+//		{
+//			writeCar = readCarFromFile(writeCar, readFromTempFile);
+//			writeInFile(writeCar, writeInOriginalFile);
+//		}
+//		fclose(readFromTempFile);
+//		fclose(writeInOriginalFile);
+//	}
+//	return;
+//}
+
+void searchCar(Car car, int searchCarID) {
+	FILE* readFile = fopen("cars.txt", "r");
+	if (readFile != NULL)
 	{
-		printf("\nCars: \n");
-		while (!feof(file))
+		//printf("\nCars: \n");
+		while (!feof(readFile))
 		{
-			fscanf(file, "\nCar ID: %d\n", &car.carID);
-			if (car.carID != carID)
+			car = readCarFromFile(car, readFile);
+			if (car.carID == searchCarID)
 			{
-				fscanf(file, "Make: %s\n", car.make);
-				fscanf(file, "Model: %s\n", car.model);
-				fscanf(file, "Year: %d\n", &car.year);
-				fscanf(file, "Horsepower: %d\n", &car.horsepower);
-				fscanf(file, "Kilometers: %d\n", &car.mileage);
-				fscanf(file, "Fuel: %s\n", car.fuelType);
-				fscanf(file, "Trasmission: %s\n", car.gearbox);
-				fscanf(file, "Was in accident? %s\n", car.wasInAccident);
 
-				fprintf(file, "\nCar ID: %d\n", car.carID);
-				fprintf(file, "Make: %s\n", car.make);
-				fprintf(file, "Model: %s\n", car.model);
-				fprintf(file, "Year: %d\n", car.year);
-				fprintf(file, "Horsepower: %d\n", car.horsepower);
-				fprintf(file, "Kilometers: %d\n", car.mileage);
-				fprintf(file, "Fuel: %s\n", car.fuelType);
-				fprintf(file, "Trasmission: %s\n", car.gearbox);
-				fprintf(file, "Was in accident? %s\n", car.wasInAccident);
+				printCar(car);
 
-			}
-			else {
-				continue;
 			}
 		}
-		fclose(file);
-		printf("\nCar was deleted!");
+		fclose(readFile);
+	}
+	else {
+		printf("Couldn't open cars.txt\n");
+	}
+	return;
+}
+
+//Test delete car
+void deleteCar(int searchCarID, Car car) {
+	FILE* readFile = fopen("cars.txt", "r");
+	FILE* file = fopen("carsTemp2.txt", "a+");
+	if (readFile != NULL)
+	{
+		//printf("\nCars: \n");
+		while (!feof(readFile))
+		{
+			car = readCarFromFile(car, readFile);
+			if (car.carID != searchCarID)
+			{
+					fprintf(file, "\nCar ID: %d\n", car.carID);
+			}
+		}
+		fclose(readFile);
 	}
 	else {
 		printf("Couldn't open cars.txt\n");
@@ -180,21 +267,25 @@ void startMenu() {
 		case 2:
 			if (hasCars)
 			{
-				printCar(newCar);
+				printAllCars(newCar);
 			}
 			else {
-				printf("\nThere are no cars registered.");
+				printAllCars(newCar);
 			}
 			break;
 			//Delete car
 		case 3:
+			printf("\nDelete car");
 			printf("\nEnter car ID:");
 			scanf("%d", &userCarID);
 			deleteCar(userCarID, newCar);
 			break;
 			//Search car
 		case 4:
-			printf("\nNot implemented yet.");
+			printf("\Search car");
+			printf("\nEnter car ID:");
+			scanf("%d", &userCarID);
+			searchCar(newCar, userCarID);
 			break;
 			//Exit app
 		case 5:
